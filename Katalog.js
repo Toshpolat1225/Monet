cc212d762a8b688338c7313a587fdc96927ffdc0
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-02-10 16:02:30 +0500

    second

--- Katalog.js
+++ Katalog.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
     $(".filtrKatalog").click(function(){
-        $(".filtrMenu").slideToggle();
+        $(".filtrSelect").slideToggle();
+    });
+    $("#reset").on("click", function () {
+        $('#year option').prop('selected', function() {
+            return this.defaultSelected;
+        });
+        $('#stile option').prop('selected', function() {
+            return this.defaultSelected;
+        });
+        $('#money option').prop('selected', function() {
+            return this.defaultSelected;
+        });
+        $('#material option').prop('selected', function() {
+            return this.defaultSelected;
+        });
+        $('#variant option').prop('selected', function() {
+            return this.defaultSelected;
+        });
+    });
+    $(function () {
+        $('footer .top_btn').click(() => {
+            window.scroll({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            });
+        })
     });
 });
